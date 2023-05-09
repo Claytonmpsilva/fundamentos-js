282d34fded7f22063abbb19e80f98f6e78919ad8
unknown <claytonmpsilva@gmail.com>
2023-05-09 17:32:22 -0300

    exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,26 @@
+// Tudo que estiver dentro de 'aspas simples' ou "aspas duplas" e comsideredo uma string. 
+
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome e "; 
+const meuNome = "Clayton"
+console.log(citacao + meuNome)
+
+// concatenação (+)
+
+// template string ou template literal
+
+//Podemos testar a transformação/conversão do código Unicode em caractere
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
